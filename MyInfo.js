7865ebfaf4e772b790b728f1d251b5c16da99265
MerlynMShelley <46276713+MerlynMShelley@users.noreply.github.com>
2020-01-26 23:29:47 +0530

    Add files via upload

---
+++ MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MyInfo(){
+	return(
+			<div>
+			<h1>Merly M Shelley</h1>
+			<p>I am a Tech Enthusiast!!</p>
+			<ul>
+				<li>New York</li>
+				<li>Washington</li>
+				<li>California</li>
+			</ul>	
+			</div>
+		)
+}
+export default MyInfo
